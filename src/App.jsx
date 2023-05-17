071b50d796d751f6ee4b6735e039d486ce820b0b
Dustin Soos <dustinmsoos@gmail.com>
2023-05-16 21:29:19 -0400

    Intial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Nav from "./assets/nav";
+
+function App() {
+  return (
+    <>
+      <Nav />
+      <p className=" font-bold">Click on the Vite and React logos to learn more</p>
+    </>
+  );
+}
+
+export default App;
---
+++ src/assets/nav.jsx
@@ -0,0 +1,12 @@
+export default function Nav() {
+  return (
+    <nav className=" text-center">
+      <ul>
+        <a href="">Home</a>
+        <li>Skills</li>
+        <li>Projects</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+}
