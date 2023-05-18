4237d51330c54d5cb95aefc381a53ebcf24579fe
Dustin Soos <dustinmsoos@gmail.com>
2023-05-18 18:08:58 -0400

    navbar/header

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -8,13 +8,15 @@ export default function Nav() {
   }
 
   return (
-    <nav className=" mt-8">
-      <FontAwesomeIcon icon={faBars} size="lg" style={{ color: "#000000" }} onClick={toggleMenu} className=" visible md:collapse" />
-      <ul id="hamburger" className=" collapse md:visible">
+    <nav className=" mt-8  ">
+      <FontAwesomeIcon icon={faBars} size="xl" style={{ color: "#000000" }} onClick={toggleMenu} className=" visible md:collapse" />
+
+      <ul id="hamburger" className="collapse md:visible md:flex gap-12 md:items-center ">
+        <h3 className=" collapse text-4xl md:visible">DS</h3>
         <a href="/">Home</a>
-        <li>Skills</li>
-        <li>Projects</li>
-        <li>Contact</li>
+        <a href="/">Skills</a>
+        <a href="/">Projects</a>
+        <a href="/">Contact</a>
       </ul>
     </nav>
   );
