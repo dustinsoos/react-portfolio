b23e3ee5ffcb25a7ddd7c41f647b9d42901773dd
Dustin Soos <dustinmsoos@gmail.com>
2023-05-20 20:42:30 -0400

    working on project section

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Main from "./components/main";
 function App() {
   return (
     <>
-      <div className="container mx-auto px-8 md:px-16 lg:px-32 xl:px-64">
+      <div className="container mx-auto px-8 md:px-16 lg:px-32 xl:px-64 ">
         <Nav />
         <Header />
         <Main />
--- src/components/main.jsx
+++ src/components/main.jsx
@@ -9,6 +9,8 @@ import py from "../assets/python.svg";
 import db from "../assets/database-solid.svg";
 import github from "../assets/github.svg";
 import terminal from "../assets/terminal-solid.svg";
+import reactPort from "../assets/react-portfolio.jpeg";
+import littlelemon from "../assets/littlelemon2.jpeg";
 
 export default function Main() {
   return (
@@ -32,8 +34,34 @@ export default function Main() {
         </div>
       </div>
       <div className=" w-full mx-auto h-px bg-black mt-52"></div>
-      <div>
-        <div></div>
+      <div id="projects" className=" mt-48 mb-44">
+        <h2 className=" text-4xl mt-32 text-center ">Projects</h2>
+        <div className=" h-full md:h-80 mx-auto flex flex-col text-center items-center justify-around  md:flex md:flex-row mt-12 md:w-full shadow-xl shadow-black/40 rounded-2xl ">
+          <img src={reactPort} alt="" className=" w-96" />
+          <div className=" text-center flex-col  pb-8 md:pb-0">
+            <h3 className=" text-lg font-semibold underline">Portfolio Project</h3>
+            <p className=" text-md font-medium pt-4 pb-4">React ~ Tailwind</p>
+            <p className=" pb-4">A portfolio showcasing my technical skills.</p>
+            <a href="https://www.google.com/" className=" bg-black px-8 cursor-pointer text-lg text-white font-semibold hover:underline">
+              Live Site
+            </a>
+          </div>
+        </div>
+        <div className=" h-full md:h-80 mx-auto flex flex-col text-center items-center justify-around  md:flex md:flex-row mt-12 md:w-full shadow-xl shadow-black/40 rounded-2xl ">
+          <img src={littlelemon} alt="" className=" w-96" />
+          <div className=" text-center flex-col  pb-8 md:pb-0">
+            <h3 className=" text-lg font-semibold underline">Little Lemon</h3>
+            <p className=" text-md font-medium pt-4 pb-4">Semantic HTML ~ CSS</p>
+            <p className=" pb-4">
+              Based in Chicago, Illinois, Little Lemon is a family-owned Mediterranean restaurant, focused on traditional recipes served with a modern twist. I
+              practiced using Semantic HTML structure
+            </p>
+            <a href="https://www.google.com/" className=" bg-black px-8 cursor-pointer text-lg text-white font-semibold hover:underline">
+              Live Site
+            </a>
+          </div>
+        </div>
+
         <div></div>
         <div></div>
       </div>
