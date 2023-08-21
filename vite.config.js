a69cb1b5faf8790421ed0e40e01c7ef25b757edf
Dustin Soos <dustinmsoos@gmail.com>
2023-08-20 21:11:46 -0400

    change

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://dustinmsoos.com/",
+  base: "https://dustinsoos.github.io/react-portfolio/",
   plugins: [react()],
 });
